921649aabe4a9cfdcec8befd55269198476d562e
victorcaldas <contatos.caldas@gmail.com>
2018-05-14 16:19:13 -0300

    Adicionando uma classe explicando variaveis e limpando nosso hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,30 +1,5 @@
 public class HelloWorld {
 
-    // Variavel String ********
-
-    public String nome;
-    public char umaLetrinha;
-
-    // Variavel Inteiros ********
-
-    private int meusNumeros;     // Int: 4 bytes.
-    public  byte apenasumByte;  //  Byte: 1 byte.
-    public  short numeroCurto; //   Short: 2 bytes.
-    public  long numerlongo;  //    Long: 8 bytes.
-
-    // Variavel Floats ********
-
-    private float numeroQuebrado; // Float: 4 bytes.
-    private double numeroSoma;   //  Double: 8 bytes
-
-    // Variavel Boolean ********
-
-    Boolean duvida;
-
-    // Variavel Constantes ********
-    final float PI = 3.1416F;
-    final String NOME_DO_PROFESSOR = "Victor";
-
     public static void main(String[] args) {
         // ele vai ˜printar˜ Hello, World na janela do terminal
         System.out.println("Hello, World");
---
+++ src/metodos/Variaveis.java
@@ -0,0 +1,29 @@
+package metodos;
+
+public class Variaveis {
+
+    // Variavel String ********
+
+    public String nome;
+    public char umaLetrinha;
+
+    // Variavel Inteiros ********
+
+    private int meusNumeros;     // Int: 4 bytes.
+    public  byte apenasumByte;  //  Byte: 1 byte.
+    public  short numeroCurto; //   Short: 2 bytes.
+    public  long numerlongo;  //    Long: 8 bytes.
+
+    // Variavel Floats ********
+
+    private float numeroQuebrado; // Float: 4 bytes.
+    private double numeroSoma;   //  Double: 8 bytes
+
+    // Variavel Boolean ********
+
+    Boolean duvida;
+
+    // Variavel Constantes ********
+    final float PI = 3.1416F;
+    final String NOME_DO_PROFESSOR = "Victor";
+}
