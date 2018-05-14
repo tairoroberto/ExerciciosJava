1c3b40bb2b5a88fa86b484f0a8646d0b3247ef5c
victorcaldas <contatos.caldas@gmail.com>
2018-05-14 16:28:39 -0300

    Organizando Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         // ele vai ˜printar˜ Hello, World na janela do terminal
         System.out.println("Hello, World");
-
     }
 }
